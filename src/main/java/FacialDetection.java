13a5d7ec3c03e7013c7d04e9ea6affe1e9cbea38
Jason Tan <jct96@cs.washington.edu>
2017-12-21 08:49:18 +0700

    added train api

--- src/main/java/FacialDetection.java
+++ src/main/java/FacialDetection.java
@@ -1,5 +1,9 @@
+import clarifai2.api.BaseClarifaiClient;
 import clarifai2.api.ClarifaiClient;
 import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.request.concept.AddConceptsRequest;
+import clarifai2.api.request.input.AddInputsRequest;
+import clarifai2.api.request.model.TrainModelRequest;
 import clarifai2.dto.input.ClarifaiImage;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.output.ClarifaiOutput;
@@ -8,9 +12,11 @@ import clarifai2.dto.prediction.Prediction;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FacialDetection {
+    public static final String modelID = "Facial Detection";
 
     private ClarifaiClient client;
 
@@ -27,7 +33,7 @@ public class FacialDetection {
                 .executeSync().get();
     }
 
-    public static Concept getBestConcept(ClarifaiOutput<Prediction> prediction) {
+    public Concept getBestConcept(ClarifaiOutput<Prediction> prediction) {
         Concept bestConcept = null;
         double max = 0;
 
@@ -40,5 +46,34 @@ public class FacialDetection {
             }
         }
         return bestConcept;
+    }
+
+    public boolean addConcept(Concept c, List<BufferedImage> listInput) throws IOException {
+        if (listInput == null || listInput.size() < 10) {
+            return false;
+        }
+
+        // add concept to model API
+        AddConceptsRequest acr = new AddConceptsRequest((BaseClarifaiClient) this.client);
+        acr.plus(c);
+
+        // add concept to BI and create ClaifaiInput
+        List<ClarifaiInput> listClarifaiInput = new ArrayList<ClarifaiInput>();
+        for(BufferedImage bi : listInput) {
+            byte[] imageInByte = util.BImageToByte(bi);
+            ClarifaiInput cur = ClarifaiInput.forImage(imageInByte);
+            cur = cur.withConcepts(c);
+            listClarifaiInput.add(cur);
+        }
+
+        // add listInput to model API
+        AddInputsRequest air = new AddInputsRequest((BaseClarifaiClient) this.client);
+        air.plus(listClarifaiInput);
+
+        // retrain model API
+        TrainModelRequest tmr = new TrainModelRequest((BaseClarifaiClient) this.client, modelID);
+        tmr.executeSync();
+
+        return true;
     }
 }
